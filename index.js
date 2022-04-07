0135021a163ced49db5b52089d36eb1bd29ed12a
Wilson Espina <wilson.espina@itv.com>
2022-04-07 19:54:59 +0100

    feat: adds aside and colors

--- index.js
+++ index.js
@@ -30,27 +30,27 @@ app.use(express.static(__dirname));
 app.listen(port, () => console.log(`App listening to port ${port}`));
 
 
-// function render() {
-// 	const css = fs.readFileSync(__dirname + "./style.css", "utf-8");
-// 	const tpl = fs.readFileSync(__dirname + "./layouts/main.hbs", "utf-8");
-// 	const partialsDir = path.join(__dirname, 'views/partials');
-// 	const filenames = fs.readdirSync(partialsDir);
+function render() {
+	const css = fs.readFileSync(__dirname + "./style.css", "utf-8");
+	const tpl = fs.readFileSync(__dirname + "./layouts/main.hbs", "utf-8");
+	const partialsDir = path.join(__dirname, 'views/partials');
+	const filenames = fs.readdirSync(partialsDir);
 
-// 	filenames.forEach(function (filename) {
-// 	  const matches = /^([^.]+).hbs$/.exec(filename);
-// 	  if (!matches) {
-// 	    return;
-// 	  }
-// 	  const name = matches[1];
-// 	  const filepath = path.join(partialsDir, filename)
-// 	  const template = fs.readFileSync(filepath, 'utf8');
+	filenames.forEach(function (filename) {
+	  const matches = /^([^.]+).hbs$/.exec(filename);
+	  if (!matches) {
+	    return;
+	  }
+	  const name = matches[1];
+	  const filepath = path.join(partialsDir, filename)
+	  const template = fs.readFileSync(filepath, 'utf8');
 
-// 	  Handlebars.registerPartial(name, template);
-// 	});
-// 	return Handlebars.compile(tpl)({
-// 		css: css,
-// 		resume: resumeJson
-// 	});
-// }
+	  Handlebars.registerPartial(name, template);
+	});
+	return Handlebars.compile(tpl)({
+		css: css,
+		resume: resumeJson
+	});
+}
 
-// module.exports = render;
+module.exports = render;
